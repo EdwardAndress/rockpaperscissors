403c07dd609ced2651bf2ea43cd71803dc5ec0bb
Edward J. Andress <eddie_andress@hotmail.com>
2014-07-26 11:14:17 +0100

    NewRelic error resolved

--- server.rb
+++ server.rb
@@ -2,10 +2,6 @@ require 'sinatra/base'
 require './lib/player'
 require './lib/game'
 
-configure :production do
-  require 'newrelic_rpm'
-end
-
 
 class RockPaperScissors < Sinatra::Base
   get '/' do
